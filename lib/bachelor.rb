511adf992b199a1a42d7a7fb9ded571eb875214a
jameslee1187 <jameslee1187@gmail.com>
2018-10-03 19:31:27 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,7 +35,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  average = 0
+  average = 0.0
   data.each do |season, contestants|
     contestants.each do |contestant|
       
