60fac4bdc6e9698ae309cbaf5e30c47d4f5ea5f1
jameslee1187 <jameslee1187@gmail.com>
2018-10-03 19:35:41 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,7 +39,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     sum += contestant["age"].to_i
   end
-  sum / data[season].count
+  sum / data[season].count.round
 
       
 end
